dcf6eb7654544a25365f2c80e4ce068fa66c741e
gitfanax <me@shaheer.ie>
2025-09-07 19:06:54 +0100

    Made stats.py more efficient: fixing extra loop

--- stats.py
+++ stats.py
@@ -4,12 +4,11 @@ def count_string_words(count_string):
 
 def char_counter(book):
     final_dict = {}
-    words = book.split()
-    for word in words:
-        for cr in word:
-            c = cr.lower()
-            if c in final_dict:
-                final_dict[c] += 1
-            else:
-                final_dict[c] = 1
+    for cr in book:
+        c = cr.lower()
+        if c in final_dict:
+            final_dict[c] += 1
+        else:
+            final_dict[c] = 1
     return final_dict
+
