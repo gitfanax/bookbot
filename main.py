af41c7d953c4ac0d83319dbdd5b507c7646781f9
gitfanax <me@shaheer.ie>
2025-09-08 10:55:12 +0100

    D: Finished BookBot, added sorting, and final prettifying

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_string_words, char_counter
+from stats import count_string_words, char_counter, sort_dict
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -6,8 +7,22 @@ def get_book_text(path):
 
 
 def main():
-    entire_book_string = get_book_text("books/frankenstein.txt")
-    word_count = count_string_words(entire_book_string)
-    print(char_counter(entire_book_string))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    book_path = sys.argv[1] 
+    entire_book_string = get_book_text(book_path)
+    word_count = count_string_words(entire_book_string)
+    char_dict = char_counter(entire_book_string)
+    thelist = char_dict.items()
+    sorted_list = sort_dict(thelist) 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for dct in sorted_list:
+        print(f"{dct['char']}: {dct['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -5,10 +5,23 @@ def count_string_words(count_string):
 def char_counter(book):
     final_dict = {}
     for cr in book:
-        c = cr.lower()
-        if c in final_dict:
-            final_dict[c] += 1
-        else:
-            final_dict[c] = 1
+        if cr.isalpha():
+            c = cr.lower()
+            if c in final_dict:
+                final_dict[c] += 1
+            else:
+                final_dict[c] = 1
     return final_dict
 
+def get_key(dictionary):
+    return dictionary["num"]
+
+def sort_dict(dict_of_chars):
+    sorted_list = []
+    for ch, cnt in dict_of_chars:
+        tmp_dict = {}
+        tmp_dict["char"] = ch
+        tmp_dict["num"] = cnt
+        sorted_list.append(tmp_dict)
+    sorted_list.sort(reverse=True, key = get_key)
+    return sorted_list
